daa9d9004ae2331095785c2f49178b0fe1de819d
mario19martinez <mariomartinez678abc@gmail.com>
2023-05-12 07:46:09 -0500

    prueba

--- client/src/components/DashboardContract/Contract.jsx
+++ client/src/components/DashboardContract/Contract.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../DashboardContract/contract.module.css"
-import { Card, Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow, Title } from "@tremor/react"
+import { MailIcon } from "@heroicons/react/solid";
+import { Card, Icon, Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow, Title } from "@tremor/react"
 
 const data = [
     {
@@ -30,35 +31,77 @@ const data = [
         email: 'programador@gmail.com',
         status: 'sin contactar'
 
-    },]
+    },
+    {
+        invoiseId: 2,
+         name:'social media lab',
+        profesion: 'profesor',
+         nivel: '2',
+         telefono: '12345',
+         email: 'henry@gmail.com',
+         status: 'sin contactar'
+    },
+    {
+        invoiseId: 2,
+         name:'social media lab',
+        profesion: 'profesor',
+         nivel: '2',
+         telefono: '12345',
+         email: 'henry@gmail.com',
+         status: 'sin contactar'
+    },
+    {
+        invoiseId: 2,
+         name:'social media lab',
+        profesion: 'profesor',
+         nivel: '2',
+         telefono: '12345',
+         email: 'henry@gmail.com',
+         status: 'sin contactar'
+    },
+        {
+       invoiseId: 2,
+        name:'social media lab',
+       profesion: 'profesor',
+        nivel: '2',
+        telefono: '12345',
+        email: 'henry@gmail.com',
+        status: 'sin contactar'
+    },
+]
 
 const DashboardContract = () => {
     return(
         <>
-        <Card>
-            <Title></Title>
-            <Table className="">
-            <TableHead>
-                <TableRow className="bg-lime">
-                    <TableHeaderCell className=" text-left font-medium">Invoice Id</TableHeaderCell>
-                    <TableHeaderCell className=" text-left font-medium">Name</TableHeaderCell>
-                    <TableHeaderCell className=" text-left font-medium">Profesion</TableHeaderCell>
-                    <TableHeaderCell className=" text-left font-medium">Nivel</TableHeaderCell>
-                    <TableHeaderCell className=" text-left font-medium">Telefono</TableHeaderCell>
-                    <TableHeaderCell className=" text-left font-medium">Email</TableHeaderCell>
-                    <TableHeaderCell className=" text-left font-medium">Status</TableHeaderCell>
+        <Card className="w-4/5 h-screen ">
+            <div className="flex justify-between items-center m-5">
+            <Title className={styles.title}>Dashboard</Title>
+            </div>
+
+            <Table className={styles.table}>
+            <TableHead  className={styles.tableHead}>
+                <TableRow className={styles.tableRow}>
+                    <TableHeaderCell className="text-start">Invoice Id</TableHeaderCell>
+                    <TableHeaderCell className="text-start">Name</TableHeaderCell>
+                    <TableHeaderCell className="text-start">Profesion</TableHeaderCell>
+                    <TableHeaderCell className="text-start">Nivel</TableHeaderCell>
+                    <TableHeaderCell className="text-start">Telefono</TableHeaderCell>
+                    <TableHeaderCell className="text-start">Email</TableHeaderCell>
+                    <TableHeaderCell className="text-start">Status</TableHeaderCell>
                 </TableRow>
             </TableHead>
-            <TableBody>
+
+            <TableBody className={styles.tableBody}>
                 {data.map((item) => (
-                    <TableRow className={`${styles.styles}`}>
-                        <TableCell className=" text-left">{item.invoiseId}</TableCell>
-                        <TableCell className=" text-left">{item.name}</TableCell>
-                        <TableCell className=" text-left">{item.profesion}</TableCell>
-                        <TableCell className=" text-left">{item.nivel}</TableCell>
-                        <TableCell className=" text-left">{item.telefono}</TableCell>
-                        <TableCell className=" text-left">{item.email}</TableCell>
-                        <TableCell className=" text-left">{item.status}</TableCell>
+                    <TableRow className={styles.tableCards}>
+                        <TableCell className="flex justify-start items-center p-0">{item.invoiseId}</TableCell>
+                        <TableCell className="flex justify-start items-center p-1">{item.name}</TableCell>
+                        <TableCell className="flex justify-start items-center p-2">{item.profesion}</TableCell>
+                        <TableCell className={styles.nivel}>{item.nivel}</TableCell>
+                        <TableCell className="flex justify-start items-center p-4">{item.telefono}</TableCell>
+                        <Icon size="xs" variant="solid" icon={MailIcon}/>
+                        <TableCell className="flex justify-start items-center p-5">{item.email}</TableCell>
+                        <TableCell className={styles.status}>{item.status}</TableCell>
                     </TableRow>
                 ))}
             </TableBody>
