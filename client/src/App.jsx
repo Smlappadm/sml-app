c265867cd81134637de8c826b215d6fbefbbff4d
Ozymandia-s <110313394+Ozymandia-s@users.noreply.github.com>
2023-05-24 02:27:00 -0500

    Nuevo componente ReturnPage, y funcion isRoleAllowed
    Se ha creado un nuevo componente que redirige automáticamente a la página anterior cuando el usuario intenta acceder a una ruta no permitida. Además, se ha implementado una función que define y compara el rol del usuario con el rol requerido para acceder a ciertas rutas. El objetivo es garantizar que solo los usuarios con el rol adecuado puedan acceder a esas rutas específicas.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import  styles from "./App.module.css"
+import styles from "./App.module.css";
 import Landing from "./views/Landing/Landing";
 import Lideres from "./components/Lideres/Lideres";
 import Analytics from "./views/Analytics/Analytics.jsx";
 import Settings from "./views/Settings/Settings.jsx";
 import Login from "./views/Login/Login";
+import ReturnToPage from "./components/ReturnPage/ReturnPage";
 import CorredoresDashboard from "./components/Corredores/Dashboard/CorredoresDashboard";
 import VendedoresDashboard from "./components/Vendedores/Dashboard/VendedoresDashboard";
 import { AnalyticLeader } from "./components/Lideres/Analytic/AnalyticLeader";
@@ -24,7 +25,6 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import Clevel from "./components/C-Level/Clevel";
 import Analytic from "./components/C-Level/Analytics/Analytic";
 import Incidences from "./components/Lideres/incidences/incidencias";
-import { useSelector } from "react-redux";
 
 if (!"pk_test_Z3VpZGVkLWtvZGlhay0xMi5jbGVyay5hY2NvdW50cy5kZXYk") {
   throw new Error("Missing Publishable Key");
@@ -51,11 +51,12 @@ function ProtectedPage() {
 
 function ClerkProviderWithRoutes() {
   const navigate = useNavigate();
-  const role = useSelector(state => state.rol);
-  console.log(role);
+  const role = useSelector((state) => state.rol);
 
-  const role = useSelector(state => state.rol);
-  const access = useSelector(state => state.isEmployee);
+  function isRoleAllowed(role) {
+    const allowedRoles = ["vendedor", "clevel", "leader"];
+    return allowedRoles.includes(role);
+  }
 
   return (
     <ClerkProvider publishableKey={clerkPubKey} navigate={(to) => navigate(to)}>
@@ -63,57 +64,99 @@ function ClerkProviderWithRoutes() {
         <Route path="/" element={<Login />} />
         <Route
           path="/sign-in/*"
-          element={<SignIn routing="path" path="/sign-in" appearance={{
-            variables:{
-              
-              colorInputBackground:"#222131",
-              
-              spacingUnit: "0.8rem"
-              
-            },
-            layout: {
-              socialButtonsPlacement: "top",
-            
-             
-            },
-            elements: {
-              formButtonPrimary: styles.formButtonPrimary,
-              socialButtonsBlockButton: styles.socialButtons,
-              formFieldInput : styles.formFieldInput,
-              card: styles.card,
-              main:styles.main, 
-              form: styles.form,
-              formField: styles.formField,
-              dividerRow:styles.dividerRow,
-              formFieldLabel:styles.formFieldLabel,
-              footerActionText:styles.footerActionText,
-              logoImage: styles.logoImage,
-              headerTitle: styles.headerTitle,
-              headerSubtitle: styles.headerSubtitle,
-              rootBox: styles.rootBox,
-            },
-          }}></SignIn>}/> 
-          
-        
+          element={
+            <SignIn
+              routing="path"
+              path="/sign-in"
+              appearance={{
+                variables: {
+                  colorInputBackground: "#222131",
+
+                  spacingUnit: "0.8rem",
+                },
+                layout: {
+                  socialButtonsPlacement: "top",
+                },
+                elements: {
+                  formButtonPrimary: styles.formButtonPrimary,
+                  socialButtonsBlockButton: styles.socialButtons,
+                  formFieldInput: styles.formFieldInput,
+                  card: styles.card,
+                  main: styles.main,
+                  form: styles.form,
+                  formField: styles.formField,
+                  dividerRow: styles.dividerRow,
+                  formFieldLabel: styles.formFieldLabel,
+                  footerActionText: styles.footerActionText,
+                  logoImage: styles.logoImage,
+                  headerTitle: styles.headerTitle,
+                  headerSubtitle: styles.headerSubtitle,
+                  rootBox: styles.rootBox,
+                },
+              }}
+            ></SignIn>
+          }
+        />
+
         <Route
           path="/sign-up/*"
           element={<SignUp routing="path" path="/sign-up" />}
         />
         <Route path="*" element={<h1>error 404</h1>} />
-        <Route path="/home" element={ <Landing />} />
+        <Route path="/home" element={<Landing />} />
         <Route path="/" element={<Login />} />
-        <Route path="/lideres" element={role === "clevel" || role === "leader" ? <AnalyticLeader /> : <h1>error 404</h1>} />
-        <Route path="/lideres/analytics" element={role === "clevel" || role === "leader" ? <AnalyticLeader /> : <h1>error 404</h1>} />
-        <Route path="/lideres/analytics/incidences" element={role === "clevel" || role === "leader" ? <Incidences /> : <h1>error 404</h1>} />
-        <Route path="/clevel" element={role === "clevel" || role === "leader" ? <Clevel /> : <h1>error 404</h1>} />
-        <Route path="/clevel/analytics" element={role === "clevel" || role === "leader" ? <Analytic /> : <h1>error 404</h1>} />
-        <Route path="/corredores" element={role === "corredor" || role === "clevel" || role === "leader" ? <CorredoresDashboard /> : <h1>error 404</h1>} />
-        <Route path="/corredores/history" element={role === "corredor" || role === "clevel" || role === "leader" ? <CorredoresAnlaytics /> : <h1>error 404</h1>} />
+        <Route
+          path="/lideres"
+          element={isRoleAllowed(role) ? <AnalyticLeader /> : <ReturnToPage />}
+        />
+        <Route
+          path="/lideres/analytics"
+          element={isRoleAllowed(role) ? <AnalyticLeader /> : <ReturnToPage />}
+        />
+        <Route
+          path="/lideres/analytics/incidences"
+          element={isRoleAllowed(role) ? <Incidences /> : <ReturnToPage />}
+        />
+        <Route
+          path="/clevel"
+          element={isRoleAllowed(role) ? <Clevel /> : <ReturnToPage />}
+        />
+        <Route
+          path="/clevel/analytics"
+          element={isRoleAllowed(role) ? <Analytic /> : <ReturnToPage />}
+        />
+        <Route
+          path="/corredores"
+          element={
+            isRoleAllowed(role) ? <CorredoresDashboard /> : <ReturnToPage />
+          }
+        />
+        <Route
+          path="/corredores/history"
+          element={
+            isRoleAllowed(role) ? <CorredoresAnlaytics /> : <ReturnToPage />
+          }
+        />
         <Route path="/analytics" element={<Analytics />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="/vendedores" element={role === "vendedor" || role === "clevel" || role === "leader" ? <VendedoresDashboard /> : <h1>error 404</h1>} />
-        <Route path="/vendedores/history" element={role === "vendedor" || role === "clevel" || role === "leader" ? <VendedoresHistory /> : <h1>error 404</h1>} />
-        <Route path="/vendedores/analytics" element={role === "vendedor" || role === "clevel" || role === "leader" ? <VendedoresAnalytics /> : <h1>error 404</h1>} />
+        <Route
+          path="/vendedores"
+          element={
+            isRoleAllowed(role) ? <VendedoresDashboard /> : <ReturnToPage />
+          }
+        />
+        <Route
+          path="/vendedores/history"
+          element={
+            isRoleAllowed(role) ? <VendedoresHistory /> : <ReturnToPage />
+          }
+        />
+        <Route
+          path="/vendedores/analytics"
+          element={
+            isRoleAllowed(role) ? <VendedoresAnalytics /> : <ReturnToPage />
+          }
+        />
         <Route
           path="/protected"
           element={
@@ -134,35 +177,9 @@ function ClerkProviderWithRoutes() {
 
 function App() {
   return (
-    <div className= {styles.App} >
+    <div className={styles.App}>
       <ClerkProviderWithRoutes />
     </div>
-    // <div className="App">
-    //   <Routes>
-    //     <Route path="/home" element={<Landing />} />
-    //     <Route path="/" element={<Login />} />
-    //     <Route path="/employees" element={<Employees />} />
-    //     <Route path="/employees/analytics" element={<AnalyticLeader />} />
-    //     <Route path="/corredores" element={<CorredoresDashboard />} />
-    //     <Route path="/corredores/analytics" element={<CorredoresAnlaytics />}/>
-    //     <Route path="/analytics" element={<Analytics />} />
-    //     <Route path="/settings" element={<Settings />} />
-    //     <Route
-    //       path="/vendedores"
-    //       element={<VendedoresDashboard/>}
-    //     />
-    //     <Route path="/vendedores/analytics" element={<VendedoresHistory/>} />
-    //   </Routes>
-
-    //   {(
-    //     <div className="App flex items-center justify-center">
-    //       <img
-    //         className="opacity-20 w-4/5 mt-[2%]"
-    //         src="https://cdn.discordapp.com/attachments/1105243107555037294/1106577865698459788/White_Logo_Social_Media_Lab.png"
-    //       />
-    //     </div>
-    //   )}
-    // </div>
   );
 }
 
