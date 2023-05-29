17ced9bcf7650643b0e32225767af356789f94c3
Andres Biasutto <aquiandresbiasutto@gmail.com>
2023-05-29 19:21:12 -0300

    tocando cloudinary

--- client/src/components/UploadWidget/UploadWidget.jsx
+++ client/src/components/UploadWidget/UploadWidget.jsx
@@ -1,25 +1,49 @@
 import { useEffect, useRef } from "react";
 //import { useDispatch } from "react-redux";
-
+import { Image } from 'cloudinary-react';
 const { VITE_CLOUND_NAME, VITE_UPLOAD_PRESENT } = import.meta.env;
+import { useState } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const UploadWidget = () => {
     const cloudinaryRef = useRef();
     const widgetRef = useRef();
+    const [imageUrl, setImageUrl] = useState("");
+
 
     useEffect(() => {
         cloudinaryRef.current = window.cloudinary;
         widgetRef.current = cloudinaryRef.current.createUploadWidget({
             cloudName: VITE_CLOUND_NAME,
             uploadPreset: VITE_UPLOAD_PRESENT
-        }, function(error, result) {
-            console.log(result);
+        }, function (error, result) {
+            if (!error && result && result.event === "success") {
+                // Guardar la URL de la imagen en el estado
+                setImageUrl(result.info.secure_url);
+            }
         });
     }, [])
 
-    return(
-        <button className="flex justify-center items-center w-full" 
-        onClick={() => widgetRef.current.open()}>Upload</button>
+    return (
+        <>
+
+            {imageUrl && (
+                <Image cloudName={VITE_CLOUND_NAME} publicId={imageUrl} width="300" />
+            )}
+            <button
+                className="flex justify-center items-center w-full"
+                onClick={() => widgetRef.current.open()}
+            >
+                Upload
+            </button>
+
+            {/* <Image cloudName={VITE_CLOUND_NAME} publicId="https://res.cloudinary.com/dfbafogea/image/upload/v1685386037/vyqneodxjuihr5e4owni.png" width="300" />
+        <Image cloudName={VITE_CLOUND_NAME} publicId="https://res.cloudinary.com/dfbafogea/image/upload/v1685310197/sml-app/mrdn5xji01gny4qdrnw5.jpg" width="300" />
+        <Image cloudName={VITE_CLOUND_NAME} publicId="https://res.cloudinary.com/dfbafogea/image/upload/v1685310011/cxcniz7b8kbqcpx44kdr.jpg" width="300" /> */}
+            {/* <button className="flex justify-center items-center w-full" 
+        onClick={() => widgetRef.current.open()}>Upload</button> */}
+        </>
     )
 }
 
--- client/src/views/Settings/Settings.jsx
+++ client/src/views/Settings/Settings.jsx
@@ -2,19 +2,46 @@ import Nav from "../../components/Nav/Nav";
 import Detail from "../../components/Lideres/Employees/Detail/Detail";
 import {
   useUser,
-  useOrganization,
-  useOrganizationList,
 } from "@clerk/clerk-react";
 import { useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
+import { getAllCorredores, getAllVendedores, getAllClevel, getAllLeader } from "../../redux/actions";
+import { useDispatch } from "react-redux";
 import UploadWidget from "../../components/UploadWidget/UploadWidget"
+import { Image } from 'cloudinary-react';
+const { VITE_CLOUND_NAME } = import.meta.env;
+import { useSelector } from "react-redux";
+
 
 export default function Settings() {
   const user = useUser().user;
+  const userEmail= user?.primaryEmailAddress?.emailAddress;
+  const [profileImageUrl, setProfileImageUrl] = useState("");
+  const corredores = useSelector(state=> state.corredores);
+  const vendedores = useSelector(state=> state.vendedores);
+  const leader = useSelector(state=> state.leader);
+  const clevel = useSelector(state=> state.clevel);
+  const dispatch= useDispatch()
+  const allEmployees= [...corredores, ...vendedores, ...clevel, ...leader]
 
+  useEffect(()=>{
+    dispatch(getAllCorredores())
+    dispatch(getAllVendedores())
+    dispatch(getAllLeader())
+    dispatch(getAllClevel())
+  }, [dispatch])
+
+  const handleSaveChanges = () => {
+    // Lógica para guardar los cambios del formulario
+  };
+  const selectedEmployee = allEmployees.find(employee => employee.email === userEmail);
+
+  console.log(selectedEmployee);
+  console.log(allEmployees);
+  console.log(userEmail);
   return (
     <>
-      
+
       <Nav />
       {
         <div className="flex justify-center items-center w-full">
@@ -27,46 +54,50 @@ export default function Settings() {
                 <option value="Spanish">Spanish</option>
               </select>
             </div>
-            <UploadWidget />
+            <UploadWidget onImageUpload={setProfileImageUrl} />
+            {profileImageUrl && (
+              <img src={profileImageUrl} alt={profileImageUrl} />
+            )}
+            {/* <Image cloudName={VITE_CLOUND_NAME} publicId="https://res.cloudinary.com/dfbafogea/image/upload/v1685386037/vyqneodxjuihr5e4owni.png" width="300" /> */}
             <div>
-            <form>
-            <p className="text-24 m-5 text-white">Edit Profile</p>
-            <div className="flex flex-col gap-4 w-10/12 h-full items-center">
-              <input
-                className="bg-transparent border border-white rounded-md text-center w-2/5 shadow-sm shadow-white p-1 text-[#d1d1d1]"
-                placeholder="Name"
-                type="text"
-                id="name"
-              />
-              {/* <input
+              <form>
+                <p className="text-24 m-5 text-white">Edit Profile</p>
+                <div className="flex flex-col gap-4 w-10/12 h-full items-center">
+                  <input
+                    className="bg-transparent border border-white rounded-md text-center w-2/5 shadow-sm shadow-white p-1 text-[#d1d1d1]"
+                    placeholder="Name"
+                    type="text"
+                    id="name"
+                  />
+                  {/* <input
                 type="text"
                 id="email"
                 className="bg-transparent border border-white rounded-md text-center w-2/5 shadow-sm shadow-white p-1 text-[#d1d1d1]"
                 placeholder="Email"
               /> */}
-              <input
-                type="number"
-                id="phone"
-                className="bg-transparent border border-white rounded-md text-center w-2/5 shadow-sm shadow-white p-1 text-[#d1d1d1]"
-                placeholder="Phone"
-              />
-              <input
-                type="text"
-                id="location"
-                className="bg-transparent border border-white rounded-md text-center w-2/5 shadow-sm shadow-white p-1 text-[#d1d1d1]"
-                placeholder="Location"
-              />
-              <input
-                type="text"
-                id="status"
-                className="bg-transparent border border-white rounded-md text-center w-2/5 h-1/5 shadow-md shadow-white p-1 text-[#d1d1d1]"
-                placeholder="Status"
-              />
-              <button className="bg-[#334155] hover:bg-[#4f6686] text-white py-2 px-4 rounded-full m-5">
-                Save Changes
-              </button>
-            </div>
-            </form>
+                  <input
+                    type="number"
+                    id="phone"
+                    className="bg-transparent border border-white rounded-md text-center w-2/5 shadow-sm shadow-white p-1 text-[#d1d1d1]"
+                    placeholder="Phone"
+                  />
+                  <input
+                    type="text"
+                    id="location"
+                    className="bg-transparent border border-white rounded-md text-center w-2/5 shadow-sm shadow-white p-1 text-[#d1d1d1]"
+                    placeholder="Location"
+                  />
+                  <input
+                    type="text"
+                    id="status"
+                    className="bg-transparent border border-white rounded-md text-center w-2/5 h-1/5 shadow-md shadow-white p-1 text-[#d1d1d1]"
+                    placeholder="Status"
+                  />
+                  <button className="bg-[#334155] hover:bg-[#4f6686] text-white py-2 px-4 rounded-full m-5">
+                    Save Changes
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
           <Detail
