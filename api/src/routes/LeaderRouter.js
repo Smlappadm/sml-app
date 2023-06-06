583331b1e886beffa753ee40cdba2187b8875ab9
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-06 18:00:40 -0300

    Update LeaderRouter.js

--- api/src/routes/LeaderRouter.js
+++ api/src/routes/LeaderRouter.js
@@ -5,11 +5,13 @@ const {
 	updateLeaderHandler,
 	getLeaderByIdHandler,
 	getLeaderByNameHandler,
+	getLeaderByEmailHandler,
 } = require('../Handlers/LeaderHandlers');
 const LeaderRouter = Router();
 
 LeaderRouter.post('/', postLeaderHandler);
 LeaderRouter.get('/', getAllLeadersHandler);
+LeaderRouter.get('/email', getLeaderByEmailHandler);
 LeaderRouter.get('/name', getLeaderByNameHandler);
 LeaderRouter.get('/:id', getLeaderByIdHandler);
 LeaderRouter.put('/:id', updateLeaderHandler);
